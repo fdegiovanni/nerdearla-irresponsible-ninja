18c651f765605ccff51ae5b386f2b44a37a81893
Federico Degiovanni <federicoadegiovanni@gmail.com>
2023-09-20 00:40:35 -0300

    update Game

--- scenes/Game.js
+++ scenes/Game.js
@@ -6,13 +6,16 @@ export default class Game extends Phaser.Scene {
   init() {}
 
   create() {
-    // Agregar el fondo
-    this.background = this.add.sprite(400, 300, "background");
-    this.background.displayWidth = this.sys.game.config.width + 100;
-    this.background.displayHeight = this.sys.game.config.height + 100;
+    this.addBackground();
 
     // Aquí agregarías la lógica y los elementos del juego, como jugador, enemigos, etc.
   }
 
   update() {}
+
+  addBackground() {
+    this.background = this.add.sprite(400, 300, "background");
+    this.background.displayWidth = this.sys.game.config.width + 100;
+    this.background.displayHeight = this.sys.game.config.height + 100;
+  }
 }
