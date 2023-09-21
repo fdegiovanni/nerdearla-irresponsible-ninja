2f470d49590fd1afc175239de05298910dd5a890
Federico Degiovanni <federicoadegiovanni@gmail.com>
2023-09-21 01:24:27 -0300

    update Menu and Credits styles

--- scenes/Credits.js
+++ scenes/Credits.js
@@ -1,42 +1,76 @@
+import gameOptions from "../main.js";
+import { getGameName, getCoords } from "../utils/functions.js";
+
 export default class Credits extends Phaser.Scene {
   constructor() {
     super("credits");
+
+    Object.assign(this, gameOptions);
   }
-
-  init() {}
-
-  preload() {}
 
   create() {
-    // add color to the background
+    const gameName = getGameName(this);
+    const { centerX, centerY, height, width } = getCoords(this);
     this.cameras.main.setBackgroundColor("#000000");
-    this.add.text(250, 250, "Gracias por jugar", {
-      fontSize: "32px",
-      fill: "#fff",
-    });
 
-    this.add.text(250, 300, "Desarrollado por: [Nombre del Desarrollador]", {
-      fontSize: "24px",
-      fill: "#fff",
-    });
+    this.add
+      .text(centerX, 100, gameName, {
+        fontSize: "48px",
+        color: "#fff",
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-    this.add.text(250, 350, "[Frase de Agradecimiento]", {
-      fontSize: "24px",
-      fill: "#fff",
-    });
+    this.add
+      .text(centerX, 180, "Gracias por jugar", {
+        fontSize: "40px",
+        color: "#fff",
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-    const backButton = this.add.text(250, 400, "Volver", {
-      fontSize: "24px",
-      fill: "#fff",
-    });
+    this.add
+      .text(centerX, 250, `Desarrollado por: ${this.developer}`, {
+        fontSize: "24px",
+        color: "#fff",
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-    backButton.setInteractive({ useHandCursor: true });
+    const content = [
+      "",
+      "Este juego fue diseñado originalmente por",
+      "Emanuele Feronato",
+      "Que es un desarrollador de juegos de Italia.",
+      "https://www.emanueleferonato.com/",
+      "",
+      "Este juego fue adaptado por",
+      "Federico Degiovanni",
+    ];
 
-    backButton.on("pointerup", () => {
-      this.scene.resume("menu");
-      this.scene.stop("credits");
-    });
+    this.add
+      .text(centerX, centerY + 100, content, {
+        fontSize: "20px",
+        color: "grey",
+        align: "center",
+      })
+      .setOrigin(0.5);
+
+    const backButton = this.add
+      .image(width - 100, height - 80, "cover")
+      .setScale(0.5)
+      .setInteractive({ useHandCursor: true, pixelPerfect: true });
+
+    backButton
+      .on("pointerover", () => {
+        backButton.setScale(0.7);
+      })
+      .on("pointerout", () => {
+        backButton.setScale(0.5);
+      })
+      .on("pointerup", () => {
+        this.scene.resume("menu");
+        this.scene.stop("credits");
+      });
   }
-
-  update() {}
 }
--- scenes/Preload.js
+++ scenes/Preload.js
@@ -5,7 +5,7 @@ export default class Preload extends Phaser.Scene {
 
   preload() {
     this.load.image("background", "./assets/images/background.png");
-    this.load.image("cover", "./assets/images/cover.png");
+    this.load.image("cover", "./assets/images/cover-removebg.png");
     this.load.spritesheet("cloud", "./assets/images/cloud.png", {
       frameWidth: 256,
       frameHeight: 256,
